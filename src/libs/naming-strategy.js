879467355756ddc1af331bb82a0f7af6f691339f
fuanlei <767550758@qq.com>
2019-11-28 20:10:21 +0800

    add

---
+++ src/libs/naming-strategy.js
@@ -0,0 +1,91 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: fuanlei
+ * @Date: 2019-09-30 14:13:47
+ * @LastEditors: fuanlei
+ * @LastEditTime: 2019-11-07 17:30:49
+ */
+
+const { STR } = require("./str");
+
+/**
+ * 
+ * @param {String} input 
+ * @returns {String}
+ */
+function toPascal(input) {
+        let ret = "";
+        split(input).forEach(word => {
+                ret += STR.upperFirstLetter(word.toLowerCase());
+        });
+
+        return ret;
+}
+
+/**
+ * 
+ * @param {String} input 
+ * @returns {String}
+ */
+function toCamel(input) {
+        let ret = "";
+        split(input).forEach(word => {
+                ret += ret != "" ? STR.upperFirstLetter(word.toLowerCase()) : word.toLowerCase();
+        });
+
+        return ret;
+}
+
+/**
+ * 
+ * @param {String} input
+ * @returns {String} 
+ */
+function toHungary(input) {
+        let ret = "";
+        split(input).forEach((word, i, array) => {
+                ret += i != array.length - 1
+                        ? word.toLowerCase() + "_" : word.toLowerCase();
+        });
+
+        return ret;
+}
+
+/**
+ * @description Split to words, 1. split by '_' if contains any '_' 2. tokenlize 
+ * @param {String} input 
+ * @returns {[String]}
+ */
+function split(input) {
+        if (input.indexOf("_") != -1)
+                return input.split("_");
+
+        if (input == "")
+                throw new Error("input can not be empty!");
+
+        let word = input[0];
+        let ls = [];
+
+        for (let i = 1; i < input.length; i++) {
+                let c = input.charAt(i);
+
+                if (c >= "A" && c <= "Z") {
+                        ls.push(word);
+                        word = c;
+                } else {
+                        word += c;
+                }
+        }
+
+        ls.push(word);
+
+        return ls;
+
+}
+
+exports.NamingStrategy = {
+        toHungary,
+        toCamel,
+        toPascal
+}
