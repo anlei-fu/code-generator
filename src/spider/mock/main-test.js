c0e87af205c23282b897b81fd67043d9938b3c35
anlei fu <767550758@qq.com>
2020-08-31 22:22:23 +0800

    add

--- src/spider/mock/main-test.js
+++ src/spider/mock/main-test.js
@@ -16,24 +16,16 @@ async function run(pageContext) {
                 let href = $(e).attr("href");
                 let full = pageContext.urlResolver.resolve(href);
                 if(full&&matcher.match(full.url))
-                  console.log(full.url)
+                 builder.newUrl(full);
         });
 
        let summary= $("body > div.body-wrapper > div.content-wrapper > div > div.main-content > div.lemma-summary").text();
-
-       let syms =[];
-       $("body > div.body-wrapper > div.before-content > div.polysemant-list.polysemant-list-normal > ul").find("li").each((i,e)=>{
-
-       });
-
-       let properties =[];
-
-       console.log(text);
 
         builder.data({
                 summary,
         });
         builder.success();
+        console.log(builder.build())
 }
 
 async function main() {
@@ -41,13 +33,13 @@ async function main() {
                 .autoDownloadPage(true)
                 .downloadTimeout(10000)
                 .urlMatchPatterns(`["^(http://|https://)baike.baidu.com/item/.*"]`)
-                .urlEncodes(`{"https://baike.baidu.com/item":"#1/"}`)
+                .urlEncodes(`{"https://baike.baidu.com/item":"#1"}`)
                 .crawlType(1)
                 .build();
 
         let context = await createPageContext(
                 taskConfig,
-                { url: "https://baike.baidu.com/item/%E8%99%8E/865" }
+                { url: "https://baike.baidu.com/item/%E7%BE%8E%E5%9B%BD/125486" }
         );
 
         await run(context)
