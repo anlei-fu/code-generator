5079a4593d030118ca493e2476962ce1c29e0969
anlei fu <767550758@qq.com>
2021-06-02 21:16:28 +0800

    xiugai

--- src/render-service/RenderController.js
+++ src/render-service/RenderController.js
@@ -2,12 +2,28 @@ const { EjsRender } = require("./../generator/common/renders")
 const { Controller } = require("./../http")
 
 class EjsController extends Controller {
-       
+        constructor(){
+                this._render = new EjsRender("./template");
+                super("ejs-render")
+        }
+
+        getTemplate(){
+
+        }
+
+        updateTemplate(){
+                
+        }
+
         render({ query }) {
-                 
+                return this.resposneObject(this._render.render(query.template,query.model));
         }
 
         customerRender({ query }) {
+
+        }
+
+        _renderCore(){
 
         }
 }
